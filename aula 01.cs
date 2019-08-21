5122ec91930d12aabb31e62ee6ed9796215357aa
Gusttavohennri <44401890+Gusttavohennri@users.noreply.github.com>
2019-08-20 22:06:55 -0300

    Add files via upload

---
+++ aula 01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static string Main(string[] args)
+        {
+            int a = 12;
+            int b = 9;
+            string Result;
+                Result = (a == b ? "sim" : "não");
+            return(Result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ aula 02.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[5];
+            int[] B = new int[5];
+            int[] R = new int[5];
+
+            int Pos = 0;
+            bool Existe;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Elemento {0} do Vetor A: ", i + 1);
+                A[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Elemento {0} do Vetor B: ", i + 1);
+                B[i] = int.Parse(Console.ReadLine());
+            }
+            Console.ReadKey();
+
+            for(int i = 0; i<5;i++)
+            {
+                Existe = false;
+
+                for (int j = 0; j < 5; j++)
+                {
+                    if (A[i] == B[j])
+                        Existe = true;
+                    
+                }
+
+                if (!Existe)
+                    R[Pos++] = A[i];
+                //Pos++;
+            }
+       
+            Console.Write("\n\nElementos de A que nao existem em B: \n");
+
+            for (int i = 0; i < Pos; i++)
+            {
+                Console.WriteLine("{0}", R[i]);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
+
