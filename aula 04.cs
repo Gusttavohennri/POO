2c316592aa574b952b51bd6886936a6da82c8168
Gusttavohennri <44401890+Gusttavohennri@users.noreply.github.com>
2019-09-03 21:58:16 -0300

    Add files via upload

---
+++ aula 04.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Funcionario
+    {
+        public string  Nome { get; set; }
+        public string Cargo { get; set; }
+        public double Salario { get; set; }
+
+        public void LeDados()
+        {
+            Console.Write("\nNome do funcionario: ");
+            Nome = Console.ReadLine();
+
+            Console.Write("\nCargo................: ");
+            Cargo = Console.ReadLine();
+
+            Console.Write("\nSalario..............: ");
+            Salario = double.Parse(Console.ReadLine());
+        }
+    }
+}
